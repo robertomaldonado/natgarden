29997d4a36d4d4e1f5f996d52abb35982763c5df
Rob <rbt.maldonado@gmail.com>
2021-03-23 22:15:25 -0400

    Replaced default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,89 +1,37 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
-      fixed
       app
+      height="80"
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
+      <v-avatar
+        class="mr-3"
+        color="grey lighten-5"
+        size="50"
       >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+        <v-img
+          contain
+          max-height="70%"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        ></v-img>
+      </v-avatar>
+      <v-toolbar-title class="font-weight-black headline">
+        NATGARDEN
+      </v-toolbar-title>
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
-      :absolute="!fixed"
-      app
+      class="justify-center"
+      color="#292929"
+      height="50"
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <div class="title font-weight-light grey--text text--lighten-1 text-center">
+        &copy; NatGarden {{ (new Date()).getFullYear() }}
+      </div>
     </v-footer>
   </v-app>
 </template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - natgarden',
-    title: 'natgarden',
+    titleTemplate: 'NatGarden',
+    title: 'NatGarden',
     htmlAttrs: {
       lang: 'en'
     },
