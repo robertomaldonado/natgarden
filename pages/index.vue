24664fb00aa631a1bc8af4936040ed97ad38ffcf
Rob <rbt.maldonado@gmail.com>
2021-03-23 22:49:06 -0400

    Added templating code. Needs to be componetized.

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
+  <section>
+  <!-- <v-row justify="center" align="center"> -->
+    <!-- <v-col cols="12" sm="8" md="6"> -->
+      <!-- <div class="text-center">
         <logo />
         <vuetify-logo />
-      </div>
-      <v-card>
+      </div> -->
+      <!-- <v-card>
         <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
         </v-card-title>
@@ -71,9 +72,252 @@
             Continue
           </v-btn>
         </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+      </v-card> -->
+      <section id="hero">
+        <v-row no-gutters>
+          <v-img
+            :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+            src="https://storage.googleapis.com/natgarden-alpha/paisaje.jpg"
+          >
+            <v-theme-provider dark>
+              <v-container fill-height>
+                <v-row
+                  align="center"
+                  class="white--text mx-auto"
+                  justify="center"
+                >
+                  <v-col
+                    class="white--text text-center"
+                    cols="12"
+                    tag="h1"
+                  >
+                    <span
+                    :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+                      class="font-weight-light"
+                    >
+                      NATURAL GARDENS
+                    </span>
+                    <br>
+                    <span
+                      :class="[$vuetify.breakpoint.smAndDown ? 'display-3': 'display-4']"
+                      class="font-weight-black"
+                    >
+                      NATGAREN
+                    </span>
+                  </v-col>
+                  <v-btn
+                    class="align-self-end"
+                    fab
+                    outlined
+                    @click="$vuetify.goTo('#about-me')"
+                  >
+                    <v-icon>mdi-chevron-double-down</v-icon>
+                  </v-btn>
+                </v-row>
+              </v-container>
+            </v-theme-provider>
+          </v-img>
+        </v-row>
+      </section>
+      <!--  -->
+      <section id="about-me">
+        <div class="py-12"></div>
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3">TRAYECTORIA</h2>
+          <v-responsive
+            class="mx-auto mb-8"
+            width="56"
+          >
+            <v-divider class="mb-1"></v-divider>
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-responsive
+            class="mx-auto title font-weight-light mb-8"
+            max-width="720"
+          >
+          Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+          </v-responsive>
+          <!-- <v-avatar
+            class="elevation-12 mb-12"
+            size="128"
+          >
+            <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+          </v-avatar> -->
+          <div></div>
+          <v-btn
+            color="grey"
+            href="https://vuetifyjs.com"
+            outlined
+            large
+          >
+            <span class="grey--text text--darken-1 font-weight-bold">
+              Link a otro lado
+            </span>
+          </v-btn>
+        </v-container>
+        <div class="py-12"></div>
+      </section>
+      <!--  -->
+      <section
+        id="features"
+        class="grey lighten-3"
+      >
+        <div class="py-12"></div>
+
+        <v-container class="text-center">
+          <h2 class="display-2 font-weight-bold mb-3">NUESTROS TRABAJOS</h2>
+
+          <v-responsive
+            class="mx-auto mb-12"
+            width="56"
+          >
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+
+          <v-row>
+            <v-col
+              v-for="({ icon, title, text }, i) in features"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <v-card
+                class="py-12 px-4"
+                color="grey lighten-5"
+                flat
+              >
+                <v-theme-provider dark>
+                  <div>
+                    <v-avatar
+                      color="primary"
+                      size="88"
+                    >
+                      <v-icon
+                        large
+                        v-text="icon"
+                      ></v-icon>
+                    </v-avatar>
+                  </div>
+                </v-theme-provider>
+
+                <v-card-title
+                  class="justify-center font-weight-black text-uppercase"
+                  v-text="title"
+                ></v-card-title>
+
+                <v-card-text
+                  class="subtitle-1"
+                  v-text="text"
+                >
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <div class="py-12"></div>
+      </section>
+
+      <section id="stats">
+        <v-parallax
+          :height="$vuetify.breakpoint.smAndDown ? 700 : 500"
+          src="https://storage.googleapis.com/natgarden-alpha/green_view.jpg"
+        >
+          <v-container fill-height>
+            <v-row class="mx-auto">
+              <v-col
+                v-for="[value, title] of stats"
+                :key="title"
+                cols="12"
+                md="3"
+              >
+                <div class="text-center">
+                  <div
+                    class="display-3 font-weight-black mb-4"
+                    v-text="value"
+                  ></div>
+
+                  <div
+                    class="title font-weight-regular text-uppercase"
+                    v-text="title"
+                  ></div>
+                </div>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-parallax>
+      </section>
+      <!--  -->
+      <v-sheet
+        id="contact"
+        color="#333333"
+        dark
+        tag="section"
+        tile
+      >
+        <div class="py-12"></div>
+        <v-container>
+          <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">Escríbenos</h2>
+          <v-responsive
+            class="mx-auto mb-12"
+            width="56"
+          >
+            <v-divider class="mb-1"></v-divider>
+            <v-divider></v-divider>
+          </v-responsive>
+          <v-theme-provider light>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  flat
+                  label="Nombre*"
+                  solo
+                ></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-text-field
+                  flat
+                  label="Email*"
+                  solo
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  flat
+                  label="Tema*"
+                  solo
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-textarea
+                  flat
+                  label="Mensaje*"
+                  solo
+                ></v-textarea>
+              </v-col>
+              <v-col
+                class="mx-auto"
+                cols="auto"
+              >
+                <v-btn
+                  color="accent"
+                  x-large
+                >
+                  Enviar
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-theme-provider>
+        </v-container>
+
+        <div class="py-12"></div>
+      </v-sheet>
+      <!--  -->
+    </section>
 </template>
 
 <script>
@@ -81,9 +325,36 @@ import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
+  data() {
+    return {
+      features: [
+        { 
+          icon: "...", 
+          title: "...", 
+          text: "..." 
+        },
+        { 
+          icon: "...", 
+          title: "...", 
+          text: "..." 
+        },
+        { 
+          icon: "...", 
+          title: "...", 
+          text: "..." 
+        }
+      ]
+    }
+  },
   components: {
     Logo,
     VuetifyLogo
   }
 }
 </script>
+
+<style scoped>
+#id {
+ width: 100%
+}
+</style>
